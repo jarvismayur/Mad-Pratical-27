0c80b45c28da8be44feb074db45e39ded3f735a8
jarvismayur <54200398+jarvismayur@users.noreply.github.com>
2020-04-19 09:54:16 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.praticalno27;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText username, password;
+    public TextView log;
+    public Button login;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.e1);
+        password = findViewById(R.id.e2);
+        login = findViewById(R.id.but1);
+        log = findViewById(R.id.t1);
+
+        mAuth =FirebaseAuth.getInstance();
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                           log.setText("Login Successfull");
+
+                        }else{
+                            Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        });
+    }
+}
